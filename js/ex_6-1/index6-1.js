394b831f9553d1d7a223c3a1f2741cf262021f69
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-08-08 12:25:58 +0200

    used setTimeout

---
+++ js/ex_6-1/index6-1.js
@@ -0,0 +1,16 @@
+function printName(){
+    let helloName= "Hello John";
+    function inner(){
+        console.log(helloName);
+    }
+    setTimeout(inner, 1000);
+}
+printName(); 
+
+
+/*
+function printName(){
+    let helloName = " Hello John";
+    setTimeout(() => console.log(helloName), 1000);
+}
+printName();*/
