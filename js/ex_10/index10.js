e2687ccc77a5501269850fe3ffb1c7daa5959b4b
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-08-08 16:10:43 +0200

    used filter method

---
+++ js/ex_10/index10.js
@@ -0,0 +1,25 @@
+function adultFilter(persons) {
+  let adults = persons.filter(person => person.age >= 18);
+  return adults;
+
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const adults = adultFilter(persons);
+console.log(persons);
+let adultsno = adults.length;
+console.log(`there are ${adultsno} adults: `, adults);
+
+
