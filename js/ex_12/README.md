10d2c1a95707ee4116be7c6ad64a22562d3bd16e
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-08-08 22:47:09 +0200

    calculated average age using reduce aand math.round

---
+++ js/ex_12/README.md
@@ -0,0 +1,7 @@
+# Array Methods Advanced
+
+Create a function called `ageAverage` that takes in an array of people and returns the average age.
+
+**Suggestion**
+
+Look at the [reduce](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce) and [Math.round](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round) methods to solve the exercise.
---
+++ js/ex_12/index12.js
@@ -0,0 +1,21 @@
+function calculateAverageAge(persons) {
+  let average = Math.round((persons.reduce((totale, item) => totale + item.age, 0)) / persons.length);
+  return average;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
