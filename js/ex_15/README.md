102d3ba272518cf3ef8744b5354462c2964a8ebb
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-08-08 23:47:01 +0200

    class created

---
+++ js/ex_15/README.md
@@ -0,0 +1,6 @@
+# Classes
+
+Define a class called `Person` that takes in two arguments (`firstName` and `lastName`) in the constructor.
+
+const developer = new Person('Mario', 'Rossi');
+console.log(developer.firstName + " " + developer.lastName);
---
+++ js/ex_15/index15.js
@@ -0,0 +1,11 @@
+class Person{
+    constructor(firstName,lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+}
+
+const developer = new Person('Mario', 'Rossi');
+console.log(developer.firstName + " " + developer.lastName);
+console.log(developer);
