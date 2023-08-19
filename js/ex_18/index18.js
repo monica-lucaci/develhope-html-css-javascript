4458bb1f4119a7d126c29ae19fd58afbd3484d40
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-08-19 13:25:37 +0200

    ex18

---
+++ js/ex_18/index18.js
@@ -0,0 +1,11 @@
+class Person {
+  // ...
+}
+
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
+
+person.firstName = 'Maria';
+person.lastName = 'Verdi';
+console.log(person.fullName);
+//
