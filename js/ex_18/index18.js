f2347546ea0cc69a10d07060c9451bf31e3cfe0e
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-08-25 17:01:39 +0200

    setters and getters created

--- js/ex_18/index18.js
+++ js/ex_18/index18.js
@@ -1,11 +1,43 @@
 class Person {
-  // ...
+  constructor(firstName, lastName, age) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._age = age;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+  set firstName(newFirstName) {
+    this._firstName = newFirstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+}
+  set lastName(newLastName) {
+    this._lastName = newLastName;
 }
 
-const person = new Person('Mario', 'Rossi', 25);
+get age() {
+  return this._age;
+}
+
+set age(newAge){
+  this._age=newAge;
+}
+
+get fullName() {
+  return `${this._firstName} ${this._lastName}`
+}
+
+}
+
+
+const person = new Person("Mario", "Rossi", 25);
 console.log(person.fullName);
 
-person.firstName = 'Maria';
-person.lastName = 'Verdi';
+person.firstName = "Maria";
+person.lastName = "Verdi";
 console.log(person.fullName);
 //
