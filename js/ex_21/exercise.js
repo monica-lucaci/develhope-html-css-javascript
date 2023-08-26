d84eb2b00a21ad11230952edd6f5492c46613686
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-08-26 15:53:45 +0200

    swapping variables with destructuring

---
+++ js/ex_21/exercise.js
@@ -0,0 +1,8 @@
+let num1 = 10;
+let num2 = 20;
+console.log("Before swap:", num1, num2); // Before swap: 10 20
+
+// Variable swapping using destructuring
+[num1, num2] = [num2, num1];
+
+console.log("After swap:", num1, num2); // After swap: 20 10
