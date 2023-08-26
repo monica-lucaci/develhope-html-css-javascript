f37decccb429c251975cdf4b96b617d0e68531f1
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-08-26 16:08:29 +0200

    destructuringobject

---
+++ js/ex_23/index23.js
@@ -0,0 +1,13 @@
+function isAdult({age}) {
+    return age >= 18;
+  }
+  
+  const person = {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  };
+
+  
+  console.log(isAdult(person));
