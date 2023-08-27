0f955ec7b0e1338fcb1b900c61f87635f7953e9d
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-08-27 15:33:10 +0200

    promises

---
+++ js/ex_37/index37.js
@@ -0,0 +1,18 @@
+const number = 15;
+const checkNumber = new Promise((resolve,reject) => {
+    if(number>10){
+        resolve(number);
+    }
+    else {
+        reject(new Error("Number is not greater than 10."))
+    }
+})
+
+
+checkNumber
+  .then(value => {
+    console.log("The number is:", value);
+  })
+  .catch(error => {
+    console.error("Error:", error.message);
+  });
