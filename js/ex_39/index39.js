56995355f53e714e1c258ea72a8fb8daa0b0d424
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-09-07 21:31:30 +0200

    finally added

---
+++ js/ex_39/index39.js
@@ -0,0 +1,39 @@
+const isLogged = true;
+
+const checkIsLoggedPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (isLogged) {
+            const randomNum = Math.random();
+            resolve(randomNum);
+        } else {
+            reject(new Error("User is not logged in."));
+        }
+    }, 1000);
+});
+
+const checkRandomNumberPromise = (randomNum) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (randomNum > 0.5) {
+                const data = { name: "John", age: 24 };
+                resolve(data);
+            } else {
+                reject(new Error("Error: Random number is not greater than 0.5."));
+            }
+        }, 1000);
+    });
+};
+
+checkIsLoggedPromise
+    .then((randomNum) => {
+        return checkRandomNumberPromise(randomNum);
+    })
+    .then((userData) => {
+        console.log(userData);
+    })
+    .catch((error) => {
+        console.error(error.message);
+    })
+    .finally(() => {
+        console.log("Promise chain completed.");
+    });
